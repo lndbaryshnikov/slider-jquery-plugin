6e469330d1a27b4f59197f083af7dc9f428f1de4
Leonid <leobar98@mail.ru>
2019-07-08 11:29:54 +0300

    realize Presenter's getView method

--- app/src/MVP modules/presenter/presenter.js
+++ app/src/MVP modules/presenter/presenter.js
@@ -2,6 +2,12 @@ export default function Presenter(_view) {
 
     Object.defineProperties(this, {
 
+        view : {
+            get : function() {
+                return _view;
+            },
+            enumerable : false
+        }
 
     })
 }
--- app/tests/presenter.test.js
+++ app/tests/presenter.test.js
@@ -1,11 +1,11 @@
 import Presenter from '../src/MVP modules/presenter/presenter'
-
+import View from '../src/MVP modules/view/view'
 
 describe('Presenter', () => {
 
     it("getView method", () => {
 
-        const view = new Presenter().view;
+        const view = new Presenter(new View()).view;
         expect(view).to.be.an('object');
 
     });
