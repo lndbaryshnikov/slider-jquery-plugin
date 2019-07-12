082a8e5b3261652e74838ccaea150bf2ba390df0
Leonid <leobar98@mail.ru>
2019-07-12 13:41:47 +0300

    realize Model's throwing an Error when userOptions doesn't correspond the required format

--- app/src/MVP modules/model/model.js
+++ app/src/MVP modules/model/model.js
@@ -1,3 +1,25 @@
+const arrayEquals = (array_1, array_2) => {
+
+    if (!array_1 || !array_2) return false;
+
+    if (array_1.length != array_2.length) return false;
+
+    for (let i = 0; i < array_1.length; i++) {
+        // Check if we have nested arrays
+        if (array_1[i] instanceof Array && array_2[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!array_1[i].equals(array_2[i])) return false;
+        }
+        else if (array_1[i] != array_2[i]) {
+            // Warning - two different object instances
+            // will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+};
+
+
 export default function Model( userOptions ) {
 
     const defaultOptions = {
@@ -25,6 +47,12 @@ export default function Model( userOptions ) {
         }
 
         settings = $.extend(settings, userOptions);
+
+        if (!arrayEquals(Object.keys(settings), Object.keys(defaultOptions))) {
+
+            throw new Error('Options are incorrect(should correspond the required format)');
+
+        }
     }
 
     Object.defineProperties(this, {
