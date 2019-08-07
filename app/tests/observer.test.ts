39f3a0ae22827f70589c188c5359f3f04adbc64a
Leonid <leobar98@mail.ru>
2019-08-07 21:27:55 +0300

    write test for Observer removeObserver method and for throwing error when observer is not found in the list of observers

--- app/tests/observer.test.ts
+++ app/tests/observer.test.ts
@@ -25,10 +25,10 @@ describe("Observer", () => {
         it("throws an errpr when observer already in the list", () => {
 
 
-            const func_1 = () => { console.log('hello'); };
-            const func_2 = () => { console.log(); };
+            const observer_1 = () => { console.log('hello'); };
+            const observer_2 = () => {};
 
-            const testAddObserverError = (func_1: Function, func_2: Function = func_1): Function => {
+            const createFuncForTestErrors = (func_1: Function, func_2: Function = func_1): Function => {
 
                 return function() {
                     const observer = new Observer();
@@ -38,15 +38,68 @@ describe("Observer", () => {
                 }
             };
 
-            expect(testAddObserverError(func_1, func_2)).to.not.throw("Observer already in the list");
-            expect(testAddObserverError(func_1)).to.throw("Observer already in the list");
-            expect(testAddObserverError(func_2)).to.throw("Observer already in the list");
+            expect(createFuncForTestErrors(observer_1, observer_2)).to.not.throw("Observer already in the list");
+            expect(createFuncForTestErrors(observer_1)).to.throw("Observer already in the list");
+            expect(createFuncForTestErrors(observer_2)).to.throw("Observer already in the list");
         });
     });
 
     describe("removeObserver method", () => {
         it("removeObserver method works", () => {
-           // expect().();
+
+            const observer_1 = () => {
+                const thing = 5;
+                console.log(thing);
+            };
+            const observer_2 = () => {
+                const thing = 1;
+                console.log(thing);
+            };
+
+            const observer = new Observer();
+
+            observer.addObserver(function () {
+            });
+            observer.addObserver(function () {
+                console.log(5)
+            });
+            observer.addObserver(function () {
+                const item = "item";
+            });
+
+            observer.removeObserver(function () {
+                console.log(5)
+            });
+
+            expect(observer.observers.length).to.equal(2);
+            expect(observer.observers).to.deep.equal([
+                function () {
+                },
+                function () {
+                    const item = "item";
+                }
+            ]);
         });
-    })
+
+        it("throws error when observer is not found in the list", () => {
+            const observer = new Observer();
+
+            observer.addObserver(function () {
+                let y = 1;
+                y++;
+            });
+            observer.addObserver(function () {
+                console.log("hello")
+            });
+
+            const createFuncForTestError = () => {
+                observer.removeObserver(function () {
+                    let x = 1;
+                    x++;
+                })
+            };
+
+            expect(createFuncForTestError).to.throw("Could not find observer in list of observers");
+        });
+    });
 });
