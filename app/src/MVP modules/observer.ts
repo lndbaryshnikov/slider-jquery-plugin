ac1861b7a58051156c0aa22e45a41ea9eca2da14
Leonid <leobar98@mail.ru>
2019-08-07 17:25:10 +0300

    write tests for addObserver's method when it trows errors when observer is not a function and when observer is already in the list, realize this method

--- app/src/MVP modules/observer.ts
+++ app/src/MVP modules/observer.ts
@@ -7,7 +7,22 @@ interface ObserversStorage {
 class Observer implements ObserversStorage{
     observers: Function[];
 
+    constructor() {
+        this.observers = [];
+    }
+
     addObserver(observer: Function): void {
+        if(typeof observer !== 'function') {
+            throw new Error('Observer must be a function');
+        }
+
+        for (let i = 0; i < this.observers.length; i++) {
+            if (this.observers[i] === observer) {
+                throw new Error('Observer already in the list');
+            }
+        }
+
+        this.observers.push(observer);
     }
 
     removeObserver(observer: Function): void {
--- app/tests/observer.test.ts
+++ app/tests/observer.test.ts
@@ -3,11 +3,50 @@ import Observer from "../src/MVP modules/observer";
 import {expect} from 'chai';
 
 describe("Observer", () => {
-   it("addObserver method", () => {
-       const observer = new Observer();
+    describe("addObserver method", () => {
+        it("addObserver method works", () => {
+           const observer = new Observer();
 
-       observer.addObserver(function(){});
+           observer.addObserver(function(){});
 
-       expect(observer.observers.length).to.equal(1);
-   });
+           expect(observer.observers.length).to.equal(1);
+        });
+
+        it("throws an error when observer is not a function", () => {
+
+            const test = () => {
+                //@ts-ignore
+                (new Observer()).addObserver("Hello!");
+            };
+
+            expect(test).to.throw("Observer must be a function");
+        });
+
+        it("throws an errpr when observer already in the list", () => {
+
+
+            const func_1 = () => { console.log('hello'); };
+            const func_2 = () => { console.log(); };
+
+            const testAddObserverError = (func_1: Function, func_2: Function = func_1): Function => {
+
+                return function() {
+                    const observer = new Observer();
+
+                    observer.addObserver(func_1);
+                    observer.addObserver(func_2);
+                }
+            };
+
+            expect(testAddObserverError(func_1, func_2)).to.not.throw("Observer already in the list");
+            expect(testAddObserverError(func_1)).to.throw("Observer already in the list");
+            expect(testAddObserverError(func_2)).to.throw("Observer already in the list");
+        });
+    });
+
+    describe("removeObserver method", () => {
+        it("removeObserver method works", () => {
+           // expect().();
+        });
+    })
 });
