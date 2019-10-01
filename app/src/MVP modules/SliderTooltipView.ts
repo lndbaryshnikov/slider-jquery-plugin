d970febacdbd81b98e62a7a7945da8246e6f51dc
Leonid <leobar98@mail.ru>
2019-10-01 21:15:37 +0300

    write class SliderTooltipView and realized it (tests for class pass correctly)

--- app/src/MVP modules/SliderTooltipView.ts
+++ app/src/MVP modules/SliderTooltipView.ts
@@ -1,8 +1,12 @@
 export default class SliderTooltipView {
     private _text: string | number | null = null;
-    private _html: HTMLDivElement | null = null;
+    private _html: HTMLDivElement;
     private _orientation: "horizontal" | "vertical" | null = null;
     private _root: HTMLElement | null = null;
+
+    constructor() {
+        this._create();
+    }
 
     get html() {
         return this._html;
@@ -12,21 +16,33 @@ export default class SliderTooltipView {
         return this._text;
     }
 
-    init( text: string | number, orientation: "horizontal" | "vertical" ) {
-        this._text = text;
-        this._orientation = orientation;
+    init(text: string | number, orientation: "horizontal" | "vertical" ) {
+        this.setText(text);
+        this.setOrientation(orientation);
     }
 
     render(root: HTMLElement) {
         this._root = root;
+
+        this._root.append(this._html);
     }
 
     setText(text: string | number) {
         this._text = text;
+
+        this._html.innerHTML = String(text);
+    }
+
+    setOrientation(orientation: "horizontal" | "vertical") {
+        this._orientation = orientation;
+
+        this._setOrientationClass();
     }
 
     cleanTextField() {
         this._text = null;
+
+        this._html.innerHTML = "";
     }
 
     remove() {
@@ -36,8 +52,26 @@ export default class SliderTooltipView {
     destroy() {
         this.remove();
         this.cleanTextField();
-        this._html = null;
         this._orientation = null;
         this._root = null;
+
+        this._html.className = "jquery-slider-tooltip";
+    }
+
+    private _create() {
+        const tooltip = document.createElement("div");
+        tooltip.setAttribute("class", "jquery-slider-tooltip");
+
+        this._html = tooltip;
+    }
+
+    private _setOrientationClass() {
+        this._html.setAttribute("class", "jquery-slider-tooltip");
+
+        if ( this._orientation === "horizontal" ) {
+            this._html.classList.add("jquery-slider-tooltip-horizontal");
+        } else if ( this._orientation === "vertical" ) {
+            this._html.classList.add("jquery-slider-tooltip-vertical");
+        }
     }
 }
--- app/tests/SliderTooltipView/test.ts
+++ app/tests/SliderTooltipView/test.ts
@@ -3,7 +3,7 @@ import SliderTooltipView from "../../src/MVP modules/SliderTooltipView";
 describe("tooltip works correctly", () => {
     let tooltip: SliderTooltipView;
     let root: HTMLDivElement;
-    let tooltipOnDom: HTMLDivElement;
+    let horizontalTooltipOnDom: HTMLDivElement;
 
     beforeEach(() => {
         root = document.createElement("div");
@@ -11,11 +11,17 @@ describe("tooltip works correctly", () => {
         root.style.height = "5px";
         root.style.margin = "100px";
 
+        document.body.append(root);
+
         tooltip = new SliderTooltipView();
         tooltip.init(35, "horizontal");
         tooltip.render(root);
 
-        tooltipOnDom = document.querySelector(".jquery-slider-tooltip");
+        horizontalTooltipOnDom = document.querySelector(".jquery-slider-tooltip");
+    });
+
+    afterEach(() => {
+       root.remove();
     });
 
     test("tooltip exists", () => {
@@ -23,18 +29,22 @@ describe("tooltip works correctly", () => {
     });
 
     test("tooltip's value is correct", () => {
-        expect(tooltipOnDom.innerHTML).toBe("35");
+        expect(horizontalTooltipOnDom.innerHTML).toBe("35");
 
         tooltip.setText("hey");
 
-        expect(tooltipOnDom.innerHTML).toBe("hey");
+        expect(horizontalTooltipOnDom.innerHTML).toBe("hey");
     });
 
     test("rest methods", () => {
         tooltip.cleanTextField();
 
         expect(tooltip.text).toBe(null);
-        expect(tooltipOnDom.innerHTML).toBe("");
+        expect(horizontalTooltipOnDom.innerHTML).toBe("");
+
+        tooltip.setText(12345);
+
+        expect(horizontalTooltipOnDom.innerHTML).toBe("12345");
 
         tooltip.remove();
 
@@ -46,6 +56,17 @@ describe("tooltip works correctly", () => {
 
         expect(root.contains(tooltip.html)).toBe(false);
         expect(tooltip.text).toBe(null);
-        expect(tooltip.html).toBe(null);
+        expect(tooltip.html.className).toBe("jquery-slider-tooltip");
+        expect(tooltip.html.innerHTML).toBe("");
+    });
+    
+    test("setOrientation", () => {
+        expect(horizontalTooltipOnDom.className).toBe("jquery-slider-tooltip jquery-slider-tooltip-horizontal");
+
+        tooltip.setOrientation("vertical");
+        expect(horizontalTooltipOnDom.className).toBe("jquery-slider-tooltip jquery-slider-tooltip-vertical");
+
+        tooltip.setOrientation("horizontal");
+        expect(horizontalTooltipOnDom.className).toBe("jquery-slider-tooltip jquery-slider-tooltip-horizontal");
     });
 });
